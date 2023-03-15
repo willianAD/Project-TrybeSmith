6fd732b28e0884285268cffac5b6c3656a8929be
willianAD <willian.afonso013@gmail.com>
2023-03-15 18:21:09 -0300

    Requisito 7

--- src/middlewares/validateUsers.ts
+++ src/middlewares/validateUsers.ts
@@ -1,28 +1,26 @@
-// import { Request, Response, NextFunction } from 'express';
-// import Joi from 'joi';
+import { Request, Response, NextFunction } from 'express';
+import Joi from 'joi';
 
-// export default async (req: Request, res: Response, next: NextFunction) => {
-//   const user = req.body;
+export default async (req: Request, res: Response, next: NextFunction) => {
+  const user = req.body;
 
-//   const require = Joi.object({
-//     username: Joi.required(),
-//     vocation: Joi.required(),
-//     level: Joi.required(),
-//     password: Joi.required(),
-//   }).validate(user);
+  const require = Joi.object({
+    username: Joi.required(),
+    vocation: Joi.required(),
+    level: Joi.required(),
+    password: Joi.required(),
+  }).validate(user);
 
-//   const stringLength = Joi.object({
-//     name: Joi.string().min(3),
-//     amount: Joi.string().min(3),
-//   }).validate(user);
+  const typeOfAndLength = Joi.object({
+    username: Joi.string().min(3),
+    vocation: Joi.string().min(3),
+    level: Joi.number().min(1),
+    password: Joi.string().min(8),
+  }).validate(user);
 
-//   if (require.error) {
-//     return res.status(400).json({ message: require.error.message });
-//   }
+  if (require.error) return res.status(400).json({ message: require.error.message });
 
-//   if (stringLength) {
-//     return res.status(422).json({ message: stringLength.error?.message });
-//   }
+  if (typeOfAndLength) return res.status(422).json({ message: typeOfAndLength.error?.message });
 
-//   return next();
-// };
+  return next();
+};
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import UserController from '../controllers/user.controller';
 import validateLogin from '../middlewares/validateLogin';
+import ValidateUsers from '../middlewares/validateUsers';
 
 const router = Router();
 
 const userController = new UserController();
 
-router.post('/users', userController.create);
+router.post('/users', ValidateUsers, userController.create);
 
 router.post('/login', validateLogin, userController.login);
 
